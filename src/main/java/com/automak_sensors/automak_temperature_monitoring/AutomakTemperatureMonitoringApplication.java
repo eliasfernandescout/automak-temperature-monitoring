ab2073785c35b32bffd8e13ec91ff64a13e8cbc8
eliasfernandescout <eliasfernandescout@gmail.com>
2025-06-22 21:40:09 -0300

    first commit

---
+++ src/main/java/com/automak_sensors/automak_temperature_monitoring/AutomakTemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.automak_sensors.automak_temperature_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomakTemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomakTemperatureMonitoringApplication.class, args);
+	}
+
+}
