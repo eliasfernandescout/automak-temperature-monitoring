e6dcbe28868c0bb8b898a3fa77865de0f147814f
eliasfernandescout <eliasfernandescout@gmail.com>
2025-06-26 21:37:58 -0300

    Add JPA column annotation for value field in TemperatureLog entity

--- src/main/java/com/automak_sensors/automak_temperature_monitoring/domain/model/TemperatureLog.java
+++ src/main/java/com/automak_sensors/automak_temperature_monitoring/domain/model/TemperatureLog.java
@@ -1,5 +1,6 @@
 package com.automak_sensors.automak_temperature_monitoring.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ public class TemperatureLog {
 
     @Id
     private String id;
+    @Column(name = "\"value\"")
     private Double value;
     private String registeredAt;
     private String sensorId;
